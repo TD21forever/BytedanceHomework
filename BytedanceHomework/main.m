ac04042052989f183bbe74ca4431738514a32e54
TD21forever <435212619@qq.com>
2022-08-10 11:37:56 +0800

    Initial Commit

---
+++ BytedanceHomework/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BytedanceHomework
+//
+//  Created by T D on 2022/8/10.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
